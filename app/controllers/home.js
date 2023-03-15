58f1229d95b3a4c5b2fffc22eaa8b6cc007d9005
arthur <arthurfsilva110@gamial.com>
2023-03-15 16:54:11 -0300

    last commit

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,10 @@
 module.exports.index = function(application, req, res){
 
-	var connection = application.config.dbConnection();
-	var noticiasModel = new application.app.models.NoticiasDAO();
-	
-	res.render("home/index");
+	var connection = application.config.dbConnection()
+	var noticiasModel = new application.app.models.NoticiasDAO(connection)
+
+	noticiasModel.getUltimasNoticias((error, result) => {
+		res.render("home/index", {noticias : result});
+	});
+
 }
